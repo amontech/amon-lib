257aac56b75b9ba4ffdad19fbd64b53619988db7
jules goullée <julesgoullee@gmail.com>
2018-02-15 01:19:31 +0100

    ethereum compare address

--- test/unit/lib/cryptos/ethereum.spec.js
+++ test/unit/lib/cryptos/ethereum.spec.js
@@ -15,7 +15,31 @@ describe('EthereumHelper', () => {
 
     it('invalid', () => {
 
-      expect(EthereumHelper.validAddress(invalidEthereumAddress)).to.be.false;
+      expect(EthereumHelper.validAddress(invalidEthereumAddress) ).to.be.false;
+
+    });
+
+  });
+
+  describe('compareAddress', () => {
+
+    it('compare simple', () => {
+
+      expect(EthereumHelper.compareAddress(validEthereumAddress, validEthereumAddress) ).to.be.true;
+
+    });
+
+    it('compare different case', () => {
+
+      const lowerCaseAddress = '0xc1912fee45d61c87cc5ea59dae31190fffff232d';
+      expect(EthereumHelper.compareAddress(validEthereumAddress, lowerCaseAddress) ).to.be.true;
+
+    });
+
+    it('compare invalid', () => {
+
+      const otherAddress = '0xc1912fee45d61c87cd5ea59dae31190fffff232d';
+      expect(EthereumHelper.compareAddress(validEthereumAddress, otherAddress) ).to.be.false;
 
     });
 
