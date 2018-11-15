6648f83b4646a467f4260119becf4be5aab5f951
Pacien Boisson <pakokrew@gmail.com>
2018-11-14 19:39:04 -0500

    erc20

--- test/unit/lib/coins/amn.spec.js
+++ test/unit/lib/coins/amn.spec.js
@@ -1,25 +1,25 @@
-const EthereumHelper = require('../../../../lib/coins/ethereum');
+const { AMN } = require('../../../../lib/coins');
 
 const validEthereumAddress = '0xc1912fee45d61c87cc5ea59dae31190fffff232d';
 const invalidEthereumAddress = '0xC1912fEE45d61C87Cc5EA59DaE31190FFFFf232d';
 
-describe('EthereumHelper', () => {
+describe('AMNHelper', () => {
 
     before(() => {
-        this.ethereumHelper = new EthereumHelper();
+        this.amnHelper = new AMN();
     });
 
     describe('validAddress', () => {
 
     it('valid', () => {
 
-      expect(this.ethereumHelper.validAddress(validEthereumAddress)).to.be.true;
+      expect(this.amnHelper.validAddress(validEthereumAddress)).to.be.true;
 
     });
 
     it('invalid', () => {
 
-      expect(this.ethereumHelper.validAddress(invalidEthereumAddress)).to.be.false;
+      expect(this.amnHelper.validAddress(invalidEthereumAddress)).to.be.false;
 
     });
 
@@ -28,19 +28,19 @@ describe('EthereumHelper', () => {
   function addressExplorer(network, address, expectedUrl) {
 
     it(`addressExplorerUrl ${network}`, () => {
-      const helper = new EthereumHelper({network});
+      const helper = new AMN({network});
       expect(helper.addressExplorerUrl(address)).to.eq(expectedUrl);
     });
 
   }
 
-  addressExplorer('testnet', validEthereumAddress, `https://kovan.etherscan.io/address/${validEthereumAddress}`);
-  addressExplorer('mainnet', validEthereumAddress, 'https://etherscan.io/address/'+validEthereumAddress);
+  addressExplorer('testnet', validEthereumAddress, 'https://kovan.etherscan.io/token/0x5f74dfd905a1d4af90a6d9fc137d6ff97c5d7b48?a='+validEthereumAddress);
+  addressExplorer('mainnet', validEthereumAddress, 'https://etherscan.io/token/0x737f98ac8ca59f2c68ad658e3c3d8c8963e40a4c?a='+validEthereumAddress);
 
   function txExplorer(network, address, expectedUrl) {
 
     it(`txExplorerUrl ${network}`, () => {
-      const helper = new EthereumHelper({network});
+      const helper = new AMN({network});
       expect(helper.txExplorerUrl(address)).to.eq(expectedUrl);
     });
 
