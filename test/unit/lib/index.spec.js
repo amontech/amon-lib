81f54bbba7e512b82f207eba67a2a9dfb994c47d
Pacien Boisson <pakokrew@gmail.com>
2018-11-20 20:35:05 -0500

    URI

--- test/unit/lib/index.spec.js
+++ test/unit/lib/index.spec.js
@@ -4,26 +4,29 @@ const EthereumHelper = require('../../../lib/coins/ethereum');
 
 describe('module', () => {
 
-    it('AmonLib class', () => {
+  it('AmonLib class', () => {
 
-        const lib = new AmonLib();
-        expect(lib).to.exist;
-        expect(lib.opts.network).to.eq('mainnet');
-        expect(lib.coins).to.exist;
-        expect(AmonLib.crypto).to.exist;
-        expect(lib.crypto).to.exist;
+    const lib = new AmonLib();
+    expect(lib).to.exist;
+    expect(lib.opts.network).to.eq('mainnet');
+    expect(lib.coins).to.exist;
+    expect(AmonLib.crypto).to.exist;
+    expect(lib.crypto).to.exist;
 
-        const coinBTC = lib.coins('BTC');
-        expect(coinBTC).to.be.an.instanceof(BitcoinHelper);
-        expect(coinBTC.opts.network).to.eq('mainnet');
+    expect(lib.coinsInstances['BTC']).to.be.an.instanceof(BitcoinHelper);
+    expect(lib.coinsInstances['ETH']).to.be.an.instanceof(EthereumHelper);
 
-        const libTestnet = new AmonLib({network: 'testnet'});
-        expect(libTestnet.opts.network).to.eq('testnet');
+    const coinBTC = lib.coins('BTC');
+    expect(coinBTC).to.be.an.instanceof(BitcoinHelper);
+    expect(coinBTC.opts.network).to.eq('mainnet');
 
-        const coinETH = libTestnet.coins('ETH');
-        expect(coinETH).to.be.an.instanceof(EthereumHelper);
-        expect(coinETH.opts.network).to.eq('testnet');
+    const libTestnet = new AmonLib({network: 'testnet'});
+    expect(libTestnet.opts.network).to.eq('testnet');
 
-    });
+    const coinETH = libTestnet.coins('ETH');
+    expect(coinETH).to.be.an.instanceof(EthereumHelper);
+    expect(coinETH.opts.network).to.eq('testnet');
+
+  });
 
 });
