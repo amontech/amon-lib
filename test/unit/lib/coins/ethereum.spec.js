e546469b353bece400cf370af84ca042b3958398
jules goullée <julesgoullee@gmail.com>
2018-11-24 19:15:41 +0300

    eth compare address

--- test/unit/lib/coins/ethereum.spec.js
+++ test/unit/lib/coins/ethereum.spec.js
@@ -5,11 +5,11 @@ const invalidEthereumAddress = '0xC1912fEE45d61C87Cc5EA59DaE31190FFFFf232d';
 
 describe('EthereumHelper', () => {
 
-    before(() => {
-        this.ethereumHelper = new EthereumHelper();
-    });
+  before(() => {
+    this.ethereumHelper = new EthereumHelper();
+  });
 
-    describe('validAddress', () => {
+  describe('validAddress', () => {
 
     it('valid', () => {
 
@@ -20,6 +20,30 @@ describe('EthereumHelper', () => {
     it('invalid', () => {
 
       expect(this.ethereumHelper.validAddress(invalidEthereumAddress)).to.be.false;
+
+    });
+
+  });
+
+  describe('compareAddress', () => {
+
+    it('compare simple', () => {
+
+      expect(EthereumHelper.compareAddress(validEthereumAddress, validEthereumAddress) ).to.be.true;
+
+    });
+
+    it('compare different case', () => {
+
+      const lowerCaseAddress = '0xc1912fee45d61c87cc5ea59dae31190fffff232d';
+      expect(EthereumHelper.compareAddress(validEthereumAddress, lowerCaseAddress) ).to.be.true;
+
+    });
+
+    it('compare invalid', () => {
+
+      const otherAddress = '0xc1912fee45d61c87cd5ea59dae31190fffff232d';
+      expect(EthereumHelper.compareAddress(validEthereumAddress, otherAddress) ).to.be.false;
 
     });
 
