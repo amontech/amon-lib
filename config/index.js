ca53ec7ca002a0dfd47e2259be70df3c7b41eb11
cristianizzo <cristianizzo@me.com>
2022-07-20 15:56:57 +0200

    rm dotenv

--- config/index.js
+++ config/index.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
+// require('dotenv').config();
 
 const config = {
-  tronMainnet: process.env.TRON_MAINNET || 'https://api.trongrid.io/wallet',
-  shastaTestnet: process.env.SHASTA_TESTNET || 'https://api.shasta.trongrid.io/wallet',
-  nileTestnet: process.env.NILE_TESTNET || 'https://nile.trongrid.io/wallet',
+  tronMainnet: 'https://api.trongrid.io/wallet',
+  shastaTestnet: 'https://api.shasta.trongrid.io/wallet',
+  nileTestnet: 'https://nile.trongrid.io/wallet',
 };
 
 module.exports = config;
